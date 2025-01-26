224b9d56adb6abc01387382e115f6114459caea3
Abderaoufsouissi <abderaouf.souissi@outlook.com>
2025-01-26 15:10:45 +0100

    added OneToMany relationship between student and payment entities

--- src/main/java/com/example/spring_angular_app/entity/Payment.java
+++ src/main/java/com/example/spring_angular_app/entity/Payment.java
@@ -21,5 +21,7 @@ public class Payment {
     @Enumerated(EnumType.STRING)
     private PaymentStatus status;
     private String file;
+    @ManyToOne
+    private Student student;
 
 }
--- src/main/java/com/example/spring_angular_app/entity/Student.java
+++ src/main/java/com/example/spring_angular_app/entity/Student.java
@@ -3,7 +3,11 @@ package com.example.spring_angular_app.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor @NoArgsConstructor @Builder
 @Getter @Setter @ToString
@@ -16,6 +20,8 @@ public class Student {
     private String code;
     private String programId;
     private String photoUrl;
+    @OneToMany
+    private List<Payment> payments = new ArrayList<>();
 
 
 
